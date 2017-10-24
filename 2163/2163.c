65d926dc7f77795dc903f1c0afb49ef2010d1997
seokjihoon <seok8161@gmail.com>
2017-10-24 20:37:37 +0900

    2163
    2163

---
+++ 2163/2163.c
@@ -0,0 +1,12 @@
+// 아래 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하셔도 됩니다.
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+    printf("%d", (a*b)-1);
+
+    return 0;   // 정상종료 시 반드시 0을 리턴해야 합니다.
+}
+
